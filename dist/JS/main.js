7e42f896f6b9a5d24b9881d3944a6f7d339b9039
rokonroni <rokonuzzamanroni@gmail.com>
2023-06-08 19:31:36 +0600

    project done

--- dist/JS/main.js
+++ dist/JS/main.js
@@ -1,19 +1,19 @@
-const search = () =>{
+const search = () => {
     const searchbox = document.getElementById("search-item").value.toUpperCase();
-    const storeitems = document.getElementsById("product-list");
-    const product = document.querySelectorAll("product");
-    const pname = document.getElementsByTagName("h2")
+    const storeitems = document.getElementById("product-list")
+    const product = document.querySelectorAll(".product")
+    const pname = storeitems.getElementsByTagName("h2")
 
     for (var i = 0; i < pname.length; i++) {
         let match = product[i].getElementsByTagName('h2')[0];
 
         if (match) {
             let textvalue = match.textContent || match.innerHTML
-            if (textvalue.toUpperCase().indexof(searchbox) >-1) {
-                product[i].getElementsByClassName.display = "";
+            if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+                product[i].style.display = "";
             }
             else {
-                product[i].getElementsByClassName.display = "none";
+                product[i].style.display = "none";
             }
         }
     }
